2e4a8f00691ec8c8c42c63a67ba711e0585c146c
TharinduLala <tharindulalantha2001@gmail.com>
2023-12-24 09:47:08 +0530

    add readme file

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# The Kitchen Resturant
+
+Online food ordering website for a restaurant.
+
+
+## Tech Stack
+
+| Languages | Frameworks     | Database                |
+| :-------- | :------- | :------------------------- |
+| HTML,CSS,PHP | jQuery,Bootstrap | MySQL |
+
+
+
+## Preview
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## How to run
+
+Instructions to run the web page:
+
+- First you need to clone or download project.
+- Then create database and import given SQL files(`The-_Kitchen_Resturant-main\db`).
+- Make changes according to your database in `The-_Kitchen_Resturant-main\php\global.php`.
+- Run the project using `wamp server` or `xampp server`.
+
--- js/cart.js
+++ js/cart.js
@@ -1,26 +1,30 @@
 var cart = [];
 
 function addToCart(productId, productPrice) {
+
   var qty = parseInt($(`#inputProduct${productId}`).val());
   if (qty === 0 || isNaN(qty) || qty < 0) {
     alert("You need to add valid product quantity!!!");
     $(`#inputProduct${productId}`).val('');
-  } else {
-    var name = $(`#titleProduct${productId}`).text();
-    var tot = productPrice * qty;
-    var i = {
-      "productId": productId,
-      "productName": name,
-      "productPrice": productPrice,
-      "productQty": qty,
-      "productTotal": tot
-    }
-    cart.push(i);
-    $(`#inputProduct${productId}`).val('');
-    loadCartItems();
-    updateCartTotal();
   }
+
+  var name = $(`#titleProduct${productId}`).text();
+  var tot = productPrice * qty;
+  var i = {
+    "productId": productId,
+    "productName": name,
+    "productPrice": productPrice,
+    "productQty": qty,
+    "productTotal": tot
+  }
+  cart.push(i);
+  $(`#inputProduct${productId}`).val('');
+  loadCartItems();
+  updateCartTotal();
+  console.log(cart);
+
 }
+
 
 function updateCartTotal() {
   var t = calculateCartTotal();
@@ -59,11 +63,20 @@ function removeCartItem(productId) {
 function loadCartItems() {
   $("#cartList").empty();
   cart.forEach((i) => {
-    var cartItem = `<li class="list-group-item d-flex justify-content-between align-items-center">
-    ${i.productName} ${i.productQty} ${i.productTotal}
-      <span id="removeCartItem${i.productId}" class="badge"><button type="button" class="btn btn-outline-danger rounded-pill"  onclick="removeCartItem(${i.productId});"><i class="bi bi-trash"></i></button></span>
-        </li>`;
-    $("#cartList").prepend(cartItem);
+    var item = `
+    <a  class="list-group-item list-group-item-action  aria-current="true">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${i.productName}</h5>
+        <span id="removeCartItem${i.productId}" class="badge"><button type="button" class="btn btn-outline-danger rounded-pill"  onclick="removeCartItem(${i.productId});"><i class="bi bi-trash"></i></button></span>
+      </div>
+    <p  class="mb-1">  <small>Rs.${i.productPrice}.00.</small><span class="fs-5"> x  ${i.productQty}</span></p>
+      <small>Rs.${i.productTotal}.00</small>
+    </a>`;
+    $("#cartList").prepend(item);
   });
+}
+
+
+function itemIsExists(pId) {
 
 }
